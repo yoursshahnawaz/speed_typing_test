1bd272bd15b893b8873ab4e20582580b1973feb0
Yashark Awasthi <yasharkawasthi@gmail.com>
2020-05-04 20:58:02 +0530

    Added Text Test and Input

---
+++ script.py
@@ -0,0 +1,20 @@
+#Speed Typing Test - Python Project
+
+
+given_text="A quick brwon fox jumped over a lazy dog."
+print("Speed Typing Test"
+print("Type this - :")
+print(given_text)
+
+input("Press Enter Key when you are ready")
+
+start_time=time()
+
+typed_text=input()
+
+end_time=time()
+
+time_taken=end_time-start_time
+
+print("You took "+time_taken+" seconds to complete the test. ")
+
